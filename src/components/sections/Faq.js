a094426fa305f033c007973fa61a843628bd8c5b
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-01-28 22:56:58 +0000

    Update Faq.js

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -6,10 +6,30 @@ import FaqItem from '@common/FaqItem';
 import ExternalLink from '@common/ExternalLink';
 
 
-<!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/metaconsulting/15min" style="min-width:320px;height:630px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
-<!-- Calendly inline widget end -->
+class Calendly extends Component {
+  componentDidMount() {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+    script.setAttribute('src',  'https://assets.calendly.com/assets      
+                        /external/widget.js');
+    head.appendChild(script);
+  }
+  componentWillUnmount() {
+    // whatever you need to cleanup the widgets code
+  }
+  render(){
+    return (
+      <div>
+        <div id="schedule_form">
+          <div 
+            className="calendly-inline-widget"
+            data-url="https://calendly.com/username/15min"
+            style={{ minWidth: '320px', height: '580px' }} />
+        </div>
+      </div>
+    );
+  }
+}
 
 
 const FAQS = [
