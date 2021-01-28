a98154afcf82bf8d4852f5a78c94a5c422cda615
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-01-28 22:43:54 +0000

    Update About.js

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -50,10 +50,9 @@ const About = () => (
             <div>
               <h2>Speed past the competition</h2>
               <p>
-                Gatsby.js builds the fastest possible website. Instead of
-                waiting to generate pages when requested, pre-build pages and
-                lift them into a global cloud of servers — ready to be delivered
-                instantly to your users wherever they are.
+                CometLeads builds the fastest possible cold email infrastructure for your needs. Instead of
+                waiting a long onboarding process, so you can hit the ground running — 
+                with targeted high quality potential customers.
               </p>
             </div>
             <Art>
@@ -67,21 +66,20 @@ const About = () => (
             <div>
               <h2>Nothing new to learn here</h2>
               <p>
-                Enjoy the power of the latest web technologies – React.js ,
-                Webpack , modern JavaScript and CSS and more — all set up and
-                waiting for you to start building.
+                We use the power of the latest email technologies and tools,
+                and continuosly improve on our iterated models — all set up and
+                waiting for you to start getting clients.
               </p>
             </div>
           </Grid>
           <Grid>
             <div>
-              <h2>Grow and build your ideas</h2>
+              <h2>Grow and achieve your goals</h2>
               <p>
-                Waste no more time on tooling and performance. Focus on the the
-                site you want to build and nothing more.
+                Waste no more time on futile calls and uninterested parties. Focus on the growth you want and nothing more.
                 <br />
                 <br />
-                Gatsby is fast in every way that matters.
+                CometLeads gives you leads in every way that matters.
               </p>
             </div>
             <Art>
