bb6e9f68ca1a42ad270e2c19206470eadadabdc0
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-01-29 17:45:02 +0000

    Update Faq.js

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -23,32 +23,33 @@ const FAQS = [
     content: () => (
       <>
         The first thing you do is you book a meeting with us, that way we can assess your needs and establish goals and expectations.
-      If we can make sure you'll be happy with our services we will provide you with an agreed amount of potential clients every week!
-      <a href="https://calendly.com/metaconsulting/30min"><button>[Book Us Here!]</button></a>
+      If we can make sure you'll be happy with our services we will provide you with an agreed amount of potential clients every week and go from there!
+      <a href="https://calendly.com/metaconsulting/30min"><button>[You can book us here!]</button></a>
+      </>
+    ),
+  },
+  {
+    title: 'Where do you source data from and why are you better than the rest?',
+    content: () => (
+      <>
+        As for the data, we have our own database and have a network of outside specialists that we rely on for particularly niche industries. 
+      We also constantly expand our database, this ensures that we are always on top of our game. 
+      Aside from our high quality data, we have dedicated years to studying cold email approaches and changed our models accordingly, we know what works and what doesn't, we know which industries and clients to pursue or avoid.
+      Our competitive advantage resides in experience.
+      </>
+    ),
+  },
+  {
+    title: 'What is CometLeads story?',
+    content: () => (
+      <>
+        We started out as a department in our parent company and pivoted into the private agency we are today.
+        Every step of the way we were outperforming our competitors and were outgrowing our parent company's needs, so we decided to make it into it's own venture.
+        The one that many have been asking us for.
       </>
     ),
   },
   /*{
-    title: 'Where can you source data from?',
-    content: () => (
-      <>
-        We have our own database and have a network of outside 
-      </>
-    ),
-  },
-  {
-    title: 'How does do I future-proof my website?',
-    content: () => (
-      <>
-        Do not build a website with last decade’s tech. The future of the web is
-        mobile, JavaScript and APIs—the{` `}
-        <ExternalLink href="https://jamstack.org/">JAMstack</ExternalLink>.
-        Every website is a web app and every web app is a website. Gatsby.js is
-        the universal JavaScript framework you’ve been waiting for.
-      </>
-    ),
-  },
-  {
     title: 'What exactly does Gatsby build?',
     content: () => (
       <>
