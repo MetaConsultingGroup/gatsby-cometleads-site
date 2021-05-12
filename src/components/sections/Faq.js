69e9b1e09fd108756d3ec558f1bff03c49066744
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-05-12 13:13:27 +0100

    Update Faq.js

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -24,7 +24,7 @@ const FAQS = [
       <>
         The first thing you do is you book a meeting with us, that way we can assess your needs and establish goals and expectations.
       If we can make sure you'll be happy with our services we will provide you with an agreed amount of potential clients every week and go from there! &nbsp;
-      <a href="https://calendly.com/gritcult/60-min"><button>[You can book us here!]</button></a>
+      
       </>
     ),
   },
